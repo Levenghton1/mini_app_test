adc13e120ee2e7b48ded71d1985c641b56ec6137
Levenghton1 <91840150+Levenghton1@users.noreply.github.com>
2023-12-27 12:11:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     const outfitContainer = document.getElementById('outfit-container');
 
-    // Example outfits data with provided image and descriptions
+    // Example outfits data with image link and descriptions provided
     const outfits = [
         {
             imageUrl: 'https://img.ltwebstatic.com/images3_pi/2023/11/29/8a/17012205597f16183bee6edab5c089c9e67accc0c1_thumbnail_405x552.jpg',
             description: 'Elegant Evening Dress - Perfect for any formal event.',
-            buyLink: '#'
+            buyLink: 'https://your-buy-link-for-item-1.com'
         },
         {
             imageUrl: 'https://img.ltwebstatic.com/images3_pi/2023/11/29/8a/17012205597f16183bee6edab5c089c9e67accc0c1_thumbnail_405x552.jpg',
             description: 'Casual Summer Dress - Stay cool and stylish.',
-            buyLink: '#'
+            buyLink: 'https://your-buy-link-for-item-2.com'
         },
         {
             imageUrl: 'https://img.ltwebstatic.com/images3_pi/2023/11/29/8a/17012205597f16183bee6edab5c089c9e67accc0c1_thumbnail_405x552.jpg',
             description: 'Classic Office Attire - Impress in every business meeting.',
-            buyLink: '#'
+            buyLink: 'https://your-buy-link-for-item-3.com'
         }
+        // ... add more outfits if needed ...
     ];
 
     // Function to create and return an outfit element
@@ -34,22 +35,31 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Append actual outfits to the container
     outfits.forEach(outfit => {
-        const outfitElement = createOutfitElement(outfit);
-        outfitContainer.appendChild(outfitElement);
+        outfitContainer.appendChild(createOutfitElement(outfit));
     });
 
-    // Clone outfits for visual loop effect
-    outfits.forEach(outfit => {
-        const clone = createOutfitElement(outfit);
-        clone.classList.add('clone'); // Optional: add a class to cloned elements
-        outfitContainer.appendChild(clone);
-    });
+    // Optionally, remove the "Back to Chat" button from the DOM
+    const backToChatButton = document.getElementById('back-to-chat');
+    if (backToChatButton) {
+        backToChatButton.parentNode.removeChild(backToChatButton);
+    }
 
-    // Handle the back to chat button
-    document.getElementById('back-to-chat').addEventListener('click', function() {
-        // Use Telegram Web App API to close the mini app
-        if (window.Telegram.WebApp) {
-            window.Telegram.WebApp.close();
+    // Add a visual indicator for scrollable content (e.g., fading edges)
+    outfitContainer.addEventListener('scroll', function() {
+        // Check if the container is scrollable and adjust the UI accordingly
+        const maxScrollLeft = outfitContainer.scrollWidth - outfitContainer.clientWidth;
+        if (outfitContainer.scrollLeft > 0) {
+            // Add class to indicate ability to scroll left
+            outfitContainer.classList.add('can-scroll-left');
+        } else {
+            outfitContainer.classList.remove('can-scroll-left');
+        }
+
+        if (outfitContainer.scrollLeft < maxScrollLeft) {
+            // Add class to indicate ability to scroll right
+            outfitContainer.classList.add('can-scroll-right');
+        } else {
+            outfitContainer.classList.remove('can-scroll-right');
         }
     });
 });
