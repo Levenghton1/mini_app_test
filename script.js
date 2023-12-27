324276f9baeff83b4b1176432cb035739d37c9fd
Levenghton1 <91840150+Levenghton1@users.noreply.github.com>
2023-12-27 13:14:10 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     const outfitContainer = document.getElementById('outfit-container');
-    const filterButtons = document.querySelectorAll('.filter-button');
 
-    // Example outfits data with image URLs, descriptions, types, and buy links provided
+    // Example outfits data with image link and descriptions provided
     const outfits = [
         {
             imageUrl: 'https://img.ltwebstatic.com/images3_pi/2023/11/29/8a/17012205597f16183bee6edab5c089c9e67accc0c1_thumbnail_405x552.jpg',
             description: 'Elegant Evening Dress - Perfect for any formal event.',
-            type: 'casual',
             buyLink: 'https://your-buy-link-for-item-1.com'
         },
         {
             imageUrl: 'https://img.ltwebstatic.com/images3_pi/2023/11/29/8a/17012205597f16183bee6edab5c089c9e67accc0c1_thumbnail_405x552.jpg',
             description: 'Casual Summer Dress - Stay cool and stylish.',
-            type: 'work',
             buyLink: 'https://your-buy-link-for-item-2.com'
         },
         {
             imageUrl: 'https://img.ltwebstatic.com/images3_pi/2023/11/29/8a/17012205597f16183bee6edab5c089c9e67accc0c1_thumbnail_405x552.jpg',
             description: 'Classic Office Attire - Impress in every business meeting.',
-            type: 'date',
             buyLink: 'https://your-buy-link-for-item-3.com'
         }
-        // Add more outfits with types and buy links as needed
+        // ... add more outfits if needed ...
     ];
 
     // Function to create and return an outfit element
@@ -37,43 +33,30 @@ document.addEventListener('DOMContentLoaded', function() {
         return itemDiv;
     }
 
-    // Function to filter outfits based on the selected filter
-    function filterOutfits(filter) {
-        outfitContainer.innerHTML = ''; // Clear the current outfits
-
-        outfits.forEach(outfit => {
-            if (filter === 'all' || outfit.type === filter) {
-                outfitContainer.appendChild(createOutfitElement(outfit));
-            }
-        });
-    }
-
-    // Event listener for filter buttons
-    filterButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            const filter = this.getAttribute('data-filter');
-            filterButtons.forEach(btn => btn.classList.remove('active'));
-            this.classList.add('active');
-            filterOutfits(filter);
-        });
+    // Append actual outfits to the container
+    outfits.forEach(outfit => {
+        outfitContainer.appendChild(createOutfitElement(outfit));
     });
 
-    // Initial display of all outfits
-    filterOutfits('all');
+    // Optionally, remove the "Back to Chat" button from the DOM
+    const backToChatButton = document.getElementById('back-to-chat');
+    if (backToChatButton) {
+        backToChatButton.parentNode.removeChild(backToChatButton);
+    }
 
     // Add a visual indicator for scrollable content (e.g., fading edges)
     outfitContainer.addEventListener('scroll', function() {
         // Check if the container is scrollable and adjust the UI accordingly
         const maxScrollLeft = outfitContainer.scrollWidth - outfitContainer.clientWidth;
         if (outfitContainer.scrollLeft > 0) {
-            // Add class to indicate the ability to scroll left
+            // Add class to indicate ability to scroll left
             outfitContainer.classList.add('can-scroll-left');
         } else {
             outfitContainer.classList.remove('can-scroll-left');
         }
 
         if (outfitContainer.scrollLeft < maxScrollLeft) {
-            // Add class to indicate the ability to scroll right
+            // Add class to indicate ability to scroll right
             outfitContainer.classList.add('can-scroll-right');
         } else {
             outfitContainer.classList.remove('can-scroll-right');
